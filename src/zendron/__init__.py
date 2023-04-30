526b03047bdf60eaa10449561f0d577b253a1b54
mjvolk3 <mjvolk3>
2023-04-29 19:23:57 -0500

    bump version 1.1.4 -> 1.1.5

--- src/zendron/__init__.py
+++ src/zendron/__init__.py
@@ -8,7 +8,7 @@ except ModuleNotFoundError:
     import tomli as tomllib
 
 # Version of the zendron package
-__version__ = "1.1.4"
+__version__ = "1.1.5"
 
 # Read URL of the Real Python feed from config file
 # _cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
