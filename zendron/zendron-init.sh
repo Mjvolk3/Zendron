e8990be86bbed807943bad7e9f55d65b11ed6218
mjvolk3 <mjvolk3>
2024-02-17 15:25:32 -0600

    delete unused bash scripts and dendron rc in src

--- zendron/zendron-init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-eval "$(conda shell.bash hook)"
-conda activate zendron
-# TODO find a better option than clearing previous... cache should fix this.
-python src/zendron/init.py
-# don't need to see pod and should remove everytime
-echo "Initialized, check conf/config.yaml"
